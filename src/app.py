494c6efd35bfb4f33ab93334d4d2817206977af3
Okole <okole@ifz-goettingen.de>
2025-11-07 11:13:16 +0100

    troubleshoot

--- src/app.py
+++ src/app.py
@@ -74,7 +74,7 @@ from googleapiclient.http import MediaIoBaseDownload
 import json, os
 
 def download_from_drive(file_id, dest_path):
-    print(json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]), flush=True))
+    print(json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]), flush=True)
     creds = service_account.Credentials.from_service_account_file(json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]))
     print("Cred: ", creds, flush=True)
     service = build('drive', 'v3', credentials=creds)
