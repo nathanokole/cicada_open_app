77d7ff92fa2320b8449a745c4daad779e16f54df
Okole <okole@ifz-goettingen.de>
2025-11-07 10:17:24 +0100

    troubleshoot

--- src/app.py
+++ src/app.py
@@ -82,7 +82,7 @@ def download_from_drive(file_id, dest_path):
         done = False
         while not done:
             status, done = downloader.next_chunk()
-            print("Download %d%%." % int(status.progress() * 100))
+            print("Download %d%%." % int(status.progress() * 100), flush=True)
 
 @st.cache_resource(show_spinner=True)
 def load_model(path: str, file_id: str, backbone_out_: int, num_classes_: int):
